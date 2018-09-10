535adafaf7e4963bfe8f66108fbad629a4ea32d9
Edson Machado <edson.machado@grupocmc.es>
2018-09-10 13:59:52 +0200

    First project done & deployed!

--- deploy.js
+++ deploy.js
@@ -1,9 +1,22 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-	'high narrow popular feel hole napkin asset cream nerve blast wire wisdom',
-	'https://rinkeby.infura.io/v3/2b01ddc4f54f4faf852d4647f85671e7'
+	"high narrow popular feel hole napkin asset cream nerve blast wire wisdom",
+	"https://rinkeby.infura.io/v3/2b01ddc4f54f4faf852d4647f85671e7"
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log("Attempting to deploy from account", accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ["Hi there!"] })
+		.send({ gas: "1000000", from: accounts[0] });
+
+	console.log("Contract deployed to", result.options.address);
+};
+deploy();
